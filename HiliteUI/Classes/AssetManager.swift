2d4daeb88350faecdf7983b77ae29dfd0a74212e
Preston Pope <prestonwpope@gmail.com>
2017-03-06 17:00:46 -0800

    added AssetConfig

--- HiliteUI/Classes/AssetManager.swift
+++ HiliteUI/Classes/AssetManager.swift
@@ -1,22 +1,41 @@
 import Foundation
 import UIKit
 
+public struct AssetConfig {
+    static var closeButtonImageName = "icon-close"
+    static var nextButtonImageName = "icon-next"
+    static var redoButtonImageName = "icon-redo"
+    static var reviewApproveButtonImageName = "icon-review-approve"
+    static var reviewBackButtonImageName = "icon-back"
+    static var toggleCameraImageName = "icon-toggle-camera"
+    static var importFromCameraRollImageName = "icon-import-from-camera-roll"
+}
+
 open class AssetManager {
     open class func applicationBackgroundImage() -> UIImage! {
         return UIImage(named: "hilite-bg-image")
     }
     
     open class func reviewBackButtonImage() -> UIImage? {
-        return nil
+        return UIImage(named: AssetConfig.reviewApproveButtonImageName)
     }
     open class func reviewApproveButtonImage() -> UIImage? {
-        return nil
+        return UIImage(named: AssetConfig.reviewApproveButtonImageName)
     }
     
-    open class func redoButtonImage() -> UIImage? { return nil }
-    open class func closeButtonImage() -> UIImage? { return nil }
-    open class func nextButtonImage() -> UIImage? { return nil }
-    open class func toggleCameraImage() -> UIImage? { return nil }
-    open class func importFromCameraRollImage() -> UIImage? { return nil }
-
+    open class func redoButtonImage() -> UIImage? {
+        return UIImage(named: AssetConfig.redoButtonImageName)
+    }
+    open class func closeButtonImage() -> UIImage? {
+        return UIImage(named: AssetConfig.closeButtonImageName)
+    }
+    open class func nextButtonImage() -> UIImage? {
+        return UIImage(named: AssetConfig.nextButtonImageName)
+    }
+    open class func toggleCameraImage() -> UIImage? {
+        return UIImage(named: AssetConfig.toggleCameraImageName)
+    }
+    open class func importFromCameraRollImage() -> UIImage? {
+        return UIImage(named: AssetConfig.importFromCameraRollImageName)
+    }
 }
